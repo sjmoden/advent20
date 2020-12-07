d3dc3ef093c5b3ac3382c2614bc4765ac1e2b926
Sam Moden <sam.moden@spektrix.com>
2020-12-07 10:09:49 +0000

    Day 5 test fix

--- src/Day5Tests/InputCheckerTests.cs
+++ src/Day5Tests/InputCheckerTests.cs
@@ -7,35 +7,32 @@ using Tools;
 // ReSharper disable CheckNamespace
 namespace Day5Tests.BoardingPassReaderTests
 {
-    public class InputCheckerTests
+    [TestFixture]
+    public class When_running_input_checker_with_example_values
     {
-        [TestFixture]
-        public class When_running_input_checker_with_example_values
-        {
         
-            private InputChecker _sut;
+        private InputChecker _sut;
 
-            [SetUp]
-            public void SetUp()
+        [SetUp]
+        public void SetUp()
+        {
+            var inputArray = new[]
             {
-                var inputArray = new[]
-                {
-                    "FBFBBFFRLR",
-                    "FBFFBFFRLR"
+                "FBFBBFFRLR",
+                "FBFFBFFRLR"
                     
-                };
+            };
             
-                var mockPuzzleInput = new Mock<IPuzzleInput>();
-                mockPuzzleInput.Setup(p => p.GetPuzzleInputAsArray(It.IsAny<string>())).Returns(inputArray);
+            var mockPuzzleInput = new Mock<IPuzzleInput>();
+            mockPuzzleInput.Setup(p => p.GetPuzzleInputAsArray(It.IsAny<string>())).Returns(inputArray);
             
-                _sut = new InputChecker(mockPuzzleInput.Object);
-            }
+            _sut = new InputChecker(mockPuzzleInput.Object);
+        }
 
-            [Test]
-            public void Then_output_value_for_part1_is_correct()
-            {
-                Assert.That(_sut.CheckInputToGetAnswerPart1(), Is.EqualTo("357"));
-            }
+        [Test]
+        public void Then_output_value_for_part1_is_correct()
+        {
+            Assert.That(_sut.CheckInputToGetAnswerPart1(), Is.EqualTo("357"));
         }
     }
 }
