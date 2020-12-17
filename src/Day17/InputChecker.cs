575ce8051bc91cb3d5cb6804dbe1dba4f96eea65
Sam Moden <sam.moden@spektrix.com>
2020-12-17 17:34:45 +0000

    Add notes to Day 17

--- src/Day17/InputChecker.cs
+++ src/Day17/InputChecker.cs
@@ -9,6 +9,12 @@ namespace Day17
 {
     public class InputChecker:IInputChecker
     {
+        //If I was to make a change to this project I would investigate a better solution to using Vector3 and Vector4
+        //This would mean we could refactor the 3d & 4D versions of InputParser and CycleRunner to reduce duplicated code
+        //I have spent far too much time building this today so I will not be looking into this.
+        
+        //Part 2 runs fairly slowly. If I had more time I would explore how to make it more efficient (It look approx 2 mins to run)
+
         private const string InputUrl = "https://adventofcode.com/2020/day/17/input";
         private readonly ICycleRunner _cycleRunner3;
         private readonly ICycleRunner _cycleRunner4;
